20681a3b0560d218e3c33f8cc8fced480fc15fc2
Maushish Yadav <chamarnumber102@gmail.com>
2023-04-30 06:35:39 +0530

    cv

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,6 +7,8 @@ export default function HeroSection() {
 				<h1 className="text-2xl md:text-4xl mb-1 md:mb-3 text-indigo-600 dark:text-indigo-500 font-semibold">Hi, This is Maushish</h1>
         <p className='text-md md:text-xl max-w-md mb-3 text-gray-600 dark:text-gray-300'>A 19 yr old indian kid who just wants to code</p>
         <a href='#' className='inline-block px-8 py-3 border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:text-md '>See more</a>
+        <a href='#' className='inline-block px-8 py-3 border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-indigo-700 md:text-md '>My Cv</a>
+
         </div>
     </div>
   )
