41a8958700764fe7ccc1c264e01ea90f812478ad
Maushish Yadav <chamarnumber102@gmail.com>
2023-04-30 06:29:33 +0530

    removing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
 				href="mailto:webcifar@gmail.com"
 				className="text-sm md:text-md hover:text-indigo-500"
 			>
-				chamarnumber102@gmail.com
+				
 			</a>
 			<p className="text-xs mt-2 text-gray-500">
 				© By Maushish Yadav {new Date().getFullYear()}. All rights reserved
