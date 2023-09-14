5367458a08ecbe59597da89832a19206508ff93b
maushish <chamarnumber102@gmail.com>
2023-09-14 18:15:56 +0530

    Changing work section and also make it dark

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import About from './components/About';
 import Footer from './components/Footer';
 import HeroSection from './components/HeroSection';
@@ -6,48 +6,29 @@ import Services from './components/Services';
 import Works from './components/Works';
 
 function App() {
-	const [theme, setTheme] = useState(null);
+  const theme = 'dark'; // Set the theme to 'dark' permanently
 
-	useEffect(() => {
-		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-			setTheme('dark');
-		} else {
-			setTheme('light');
-		}
-	}, []);
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
 
-	const handleThemeSwitch = () => {
-		setTheme(theme === 'dark' ? 'light' : 'dark');
-	};
-
-	useEffect(() => {
-		if (theme === 'dark') {
-			document.documentElement.classList.add('dark');
-		} else {
-			document.documentElement.classList.remove('dark');
-		}
-	}, [theme]);
-
-	return (
-		<>
-			<button
-				type="button"
-				onClick={handleThemeSwitch}
-				className="fixed z-10 right-2 top-2 bg-indigo-500 text-lg p-1 rounded-md"
-			>
-				{theme === 'dark' ? '🌙' : '🌞'}
-			</button>
-			<div className="bg-white dark:bg-slate-900 min-h-screen font-inter ">
-				<div className="max-w-5xl w-11/12 mx-auto">
-					<HeroSection />
-					<Works />
-					<Services />
-					<About />
-					<Footer />
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="bg-white dark:bg-slate-900 min-h-screen font-inter">
+        <div className="max-w-5xl w-11/12 mx-auto">
+          <HeroSection />
+          <Works />
+          <Services />
+          <About />
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -33,25 +33,27 @@ function WorkItem({ imgUrl, title, tech, workUrl, githubUrl }) {
         rel="noreferrer"
         className="bg-slate-300 dark:bg-slate-800 rounded-lg overflow-hidden"
       >
-        <img
-          src={imgUrl}
-          alt="work"
-          className="w-full h-36 md:h-48 object-cover"
-        />
-        <div className="w-full p-5 text-gray-600 dark:text-gray-300">
-          <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">
-            {title}
-          </h3>
-          <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
-            {tech.map((item) => (
-              <span
-                key={item}
-                className="inline-block px-2 py-1 bg-slate-200 dark:bg-slate-900 rounded-md"
-              >
-                {item}
-              </span>
-            ))}
-          </p>
+        <div className='border-white border-x-2 border-y-2 rounded-lg'>
+          <img
+            src={imgUrl}
+            alt="work"
+            className="w-full   h-40 md:h-48 object-cover rounded-md "
+          />
+          <div className="w-full p-5 text-gray-600 dark:text-gray-300 ">
+            <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">
+              {title}
+            </h3>
+            <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
+              {tech.map((item) => (
+                <span
+                  key={item}
+                  className="inline-block px-2 py-1 bg-slate-200 dark:bg-slate-900 rounded-md"
+                >
+                  {item}
+                </span>
+              ))}
+            </p>
+          </div>
         </div>
       </a>
       {isHovered && (
